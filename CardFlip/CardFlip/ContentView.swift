190bedf4d1b41f3a20dc58dfc60290e5807bb007
Sam Foulger <samfoulger@gmail.com>
2021-01-28 19:28:55 -0800

    Sam

--- CardFlip/CardFlip/ContentView.swift
+++ CardFlip/CardFlip/ContentView.swift
@@ -9,22 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var flipped = false
-
+    
     var body: some View {
         Button(action: {
             withAnimation {
                 flipped.toggle()
             }
         }) {
-            CardView()
-                .padding()
-                .rotation3DEffect(flipped ? Angle.degrees(180.0) : Angle.degrees(0.0), axis: (x: 0, y: 1, z: 0))
+            
+            Group { if flipped {
+                CardBack()
+            } else {
+                CardFront()
+                   
+                    
+            } }
+            .rotation3DEffect(flipped ? Angle.degrees(180.0) : Angle.degrees(0.0), axis: (x: 0, y: 1, z: 0))
+            .padding()
         }
-
+        
     }
 }
 
-struct CardView: View {
+struct CardFront: View {
     var body: some View {
         ZStack {
             VStack {
@@ -42,6 +49,12 @@ struct CardView: View {
         }.font(.largeTitle)
         .background(RoundedRectangle(cornerRadius: 8).stroke(Color.primary))
         .background(RoundedRectangle(cornerRadius: 8).fill(Color.red))
+    }
+}
+
+struct CardBack: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 8).fill(Color.blue)
     }
 }
 
